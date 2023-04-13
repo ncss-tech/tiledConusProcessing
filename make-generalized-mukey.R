afbf583469c6e79dff7b9b4136bab1ec5a305418
Beaudette <dylan.beaudette@usda.gov>
2023-04-13 15:15:59 -0700

    updates

--- make-generalized-mukey.R
+++ make-generalized-mukey.R
@@ -1,8 +1,10 @@
-## Approximate gNATSGO mukey grid with a 300m modal filter. This is not aggregation, only for rapid preview of thematic maps and testing.
+## Approximate gNATSGO mukey grid with a 300m modal filter. 
+## This is not aggregation, only for rapid preview of thematic maps and testing.
+## 
 ## D.E Beaudette
 ## 2023-02-23
 
-
+library(soilDB)
 library(terra)
 
 # local files
@@ -22,5 +24,22 @@ system.time(
     overwrite = TRUE
   )
 )
+
+
+## sanity check: STATSGO mukey in aggregate version?
+s <- SDA_query("SELECT mukey FROM legend INNER JOIN mapunit ON legend.lkey = mapunit.lkey WHERE areasymbol = 'US';")
+# 9562
+nrow(s)
+
+
+# integer grid -> grid + RAT
+a <- as.factor(a)
+
+# extract rat
+rat <- cats(a)[[1]]
+
+## hmmm: 1411 STATSGO mukey remain...why?
+length(which(s$mukey %in% rat$ID))
+
 
 
